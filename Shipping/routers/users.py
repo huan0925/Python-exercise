a49a49a34d96f774ba5f2c50f8a505a46166af06
Huan0925 <judyt510ac@gmail.com>
2025-02-19 09:59:08 +0800

    Added summary & description for router.

--- Shipping/routers/users.py
+++ Shipping/routers/users.py
@@ -3,11 +3,11 @@ from fastapi import APIRouter
 router = APIRouter()
 
 # get user id
-@router.get("/{user_id}")
+@router.get("/{user_id}", summary="getUserId", description="Get User ID.")
 async def getUser(user_id: int):
     return {"message": f"Hello user {user_id}"}
 
 # create user name
-@router.post("/{username}")
+@router.post("/{username}", summary="createUser", description="Create a user.")
 async def createUser(username):
     return {"message": f"Hello user {username}"}
